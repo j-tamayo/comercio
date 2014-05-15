171c39ae587ced73ce49aee05a228abf1d1f61a9
Junior Tamayo <juni00r.tamayo@gmail.com>
2014-05-14 21:44:45 -0430

    .

--- cja_games/app/Controller/TestServiceController.php
+++ cja_games/app/Controller/TestServiceController.php
@@ -8,7 +8,7 @@ App::uses('AppController', 'Controller');
  */
 App::import('Utility', 'Xml');
 class TestServiceController extends AppController {
-
+    public $components = array('Security', 'RequestHandler');
 /**
  * Components
  *
@@ -40,7 +40,24 @@ class TestServiceController extends AppController {
        // echo '<pre>';print_r($message_text);die;
         $xml =  Xml::fromArray($message_text, array('format' => 'tags'));
 
-        echo '<pre>';print_r($xml);die;
+        //($xml);die;
+
+    }
+
+    public function getState(){
+       // http://localhost/p/repo1/public/agencias/estado/2
+       // http://localhost/p/repo1/public/estados
+
+         // remotely post the information to the server
+       /* $link =  "http://" . $_SERVER['HTTP_HOST'] . $this->webroot.'rest_phones/'.$id.'.json';
+ 
+        $data = null;
+        $httpSocket = new HttpSocket();
+        $response = $httpSocket->get($link, $data );
+        $this->set('response_code', $response->code);
+        $this->set('response_body', $response->body);
+         
+        $this -> render('/Client/request_response');*/
     }
 
 
